0471ed4e48ab486e3f44537f597162b5a17a2a74
Denis Cornehl <syphar@fastmail.fm>
2014-10-21 17:02:11 +0200

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class PyTest(Command):
 
 setup(
     name='python-briefdruckzentrum',
-    version='0.1.1',
+    version='0.1.2',
     description='a lightweight python wrapper for briefdruckzentrum.de API',
     author='codingjoe',
     url='https://github.com/Thermondo/python-briefdruckzentrum',
