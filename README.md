37b93f68e41d7326a89b0461a861cde09003c6af
Rustem Sayargaliev <r.sayargaliev@gmail.com>
2017-07-03 16:02:00 +0200

    Set explicit ciphers for ssl connection

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 [![Build Status](https://travis-ci.org/Thermondo/python-briefdruckzentrum.svg)](https://travis-ci.org/Thermondo/python-briefdruckzentrum)
 [![Coverage Status](https://img.shields.io/coveralls/Thermondo/python-briefdruckzentrum.svg)](https://coveralls.io/r/Thermondo/python-briefdruckzentrum?branch=master)
-[![License](https://pypip.in/license/python-briefdruckzentrum/badge.svg)](https://pypi.python.org/pypi/python-briefdruckzentrum/)
-[![Latest Version](https://pypip.in/version/python-briefdruckzentrum/badge.svg)](https://pypi.python.org/pypi/python-briefdruckzentrum/)
+[![PyPI](https://img.shields.io/pypi/v/python-briefdruckzentrum.svg)](https://pypi.python.org/pypi/python-briefdruckzentrum/)
 
 python-briefdruckzentrum
 ========================
--- briefdruckzentrum/adapters.py
+++ briefdruckzentrum/adapters.py
@@ -1,16 +1,16 @@
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util.ssl_ import create_urllib3_context
+
+
+CIPHERS = 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:DES-CBC3-SHA:ECDH+AES128:DH+AES:ECDH+HIGH' \
+          ':DH+HIGH:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+HIGH:RSA+3DES:!aNULL:!eNULL:!MD5'
 
 
 class SSLAdapter(HTTPAdapter):
-    """An HTTPS Transport Adapter that uses an arbitrary SSL version."""
-    def __init__(self, ssl_version=None, **kwargs):
-        self.ssl_version = ssl_version
-
-        super(SSLAdapter, self).__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(num_pools=connections,
-                                       maxsize=maxsize,
-                                       block=block,
-                                       ssl_version=self.ssl_version)
+    """Custom HTTPS Transport Adapter that uses cipher, supported by Briefdruckzentrum."""
+    def init_poolmanager(self, connections, maxsize, block, *args, **kwargs):
+        context = create_urllib3_context(ciphers=CIPHERS)
+        kwargs['ssl_context'] = context
+        return super(SSLAdapter, self).init_poolmanager(
+            connections, maxsize, block, *args, **kwargs
+        )
